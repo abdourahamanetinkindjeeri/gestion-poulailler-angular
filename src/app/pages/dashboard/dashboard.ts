ae8d39c0f196f6b2b7103b8019c996c9d5f8ad7e
tinkin <tinkin@tinkin.home>
2026-01-20 11:38:18 +0000

    Developpement de l'interface gerant les enclos avec responsivite

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,35 +1,48 @@
-import { Component } from '@angular/core';
-import {Sidebar} from '../../composants/sidebar/sidebar';
-import {Header} from '../../composants/header/header';
-import {Content} from '../../composants/content/content';
-import {Footer} from '../../composants/footer/footer';
+import { Component, OnInit } from '@angular/core';
+import { Sidebar } from '../../composants/sidebar/sidebar';
+import { Header } from '../../composants/header/header';
+import { Content } from '../../composants/content/content';
+import { Footer } from '../../composants/footer/footer';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [
-    Sidebar,
-    Header,
-    Content,
-    Footer
-  ],
+  standalone: true,
+  imports: [Sidebar, Header, Content, Footer],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrls: ['./dashboard.css'],
 })
-export class Dashboard {
-
-  sidebarOpen = true; // true = ouvert, false = réduit
+export class Dashboard implements OnInit {
+  collapsed = false;
+  sidebarOpen = false;
+  isMobile = false;
   isDark = true;
 
+  ngOnInit() {
+    this.checkScreenSize();
+    window.addEventListener('resize', () => this.checkScreenSize());
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768;
+    if (!this.isMobile) {
+      this.sidebarOpen = false;
+    }
+  }
+
   toggleSidebar() {
-    this.sidebarOpen = !this.sidebarOpen;
+    if (this.isMobile) {
+      this.sidebarOpen = !this.sidebarOpen;
+    } else {
+      this.collapsed = !this.collapsed;
+    }
+  }
+
+  closeSidebar() {
+    this.sidebarOpen = false;
   }
 
   toggleTheme() {
     this.isDark = !this.isDark;
-    if (this.isDark) {
-      document.documentElement.removeAttribute('data-theme');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
-    }
+    document.documentElement.setAttribute('data-theme', this.isDark ? 'dark' : 'light');
   }
 }
